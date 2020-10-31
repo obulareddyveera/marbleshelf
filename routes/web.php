dd143991bd4bd9997899ab691b815e6b81323979
Obulareddy Veera Bharagava Reddy <Obulareddy.Reddy@yodlee.com>
2020-10-31 16:27:18 +0530

    Setting up Laravel8

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'App\Http\Controllers\StudentController@index')->name('home');
+Route::get('/create', 'App\Http\Controllers\StudentController@create')->name('create');
+
+Route::get('/hello', function () {
+    return "Hello Veera";
+});
+Route::get('/hello/{name}', function ($name) {
+    return "Hello ".$name;
+});
